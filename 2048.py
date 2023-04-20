9b82b5b28edc2798925d44be9c4a33b2687a9e2a
Ben Rosenthal <benmrosenthal@gmail.com>
2023-04-20 18:02:53 -0400

    Setup

---
+++ 2048.py
@@ -0,0 +1,29 @@
+'''
+This is the classic game 2048 where the user works with a 4x4 area to combine numbers to reach 2048
+New numbers will be added to the board (2, 4, 8)
+Only identical numbers can be combined
+'''
+#------------------------------------------------------------------------------------------------------------------------------------
+
+from random import randint
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+def printBeginPrompt():
+    pass
+
+def gameOn():
+    pass
+
+def endPrompt():
+    pass
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+printBeginPrompt()
+
+play = True
+while play:
+    gameOn()
+
+    play = endPrompt()
