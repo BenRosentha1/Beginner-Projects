1c81304c46f80221aa992d1809fcd804825ede11
Ben Rosenthal <benmrosenthal@gmail.com>
2023-06-08 20:35:35 -0400

    commit

---
+++ GUI Shit/airQualityMonitor.py
@@ -0,0 +1,27 @@
+"""
+This is an air quality monitor using tkinter.
+"""
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Import
+from tkinter import *
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Window Declaration
+root = Tk()
+root.title("Air Quality in Columbus Ohio")
+#root.iconbitmap("Location of ICO File")
+root.geometry("750x200")
+
+# Button Functionality
+
+
+# Buttons
+
+
+# Labels
+
+
+# Pack it in
+
+
+# Main Loop
+root.mainloop()
